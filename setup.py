3fc13f6d3264888e3a48a06ef2939c0961bbe3e5
Aminu Abdul Manaf <afolanyaaminu@gmail.com>
2021-04-14 12:20:20 +0000

    Add pathlib2 to requirements.txt

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ dependencies = [dep for dep in deps if len(dep.strip()) > 0]
 
 setup(
     name="Finicky",
-    version="0.1.4",
+    version="0.1.5",
     description="Easy Data Validation",
     long_description=README,
     long_description_content_type="text/markdown",
