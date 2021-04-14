41176c0e386f237745e232d9d24b8c98c98ef4c4
Aminu Abdul Manaf <afolanyaaminu@gmail.com>
2021-04-14 09:16:23 +0000

    Fix typos in github actions file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ dependencies = [dep for dep in deps if len(dep.strip()) > 0]
 
 setup(
     name="Finicky",
-    version="0.1.3",
+    version="0.1.4",
     description="Easy Data Validation",
     long_description=README,
     long_description_content_type="text/markdown",
